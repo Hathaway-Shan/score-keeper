3a99f89b427a485581b3770915ed0dc40fc0f16c
Hathaway-Shan <hathaway.shan@gmail.com>
2022-06-14 10:43:08 -0700

    built first props function for scoreboard mostly

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 // import utilities
 
 // import component creators
-
+import createScoreboard from './components/scoreboard';
 // import state and dispatch functions
-
+import state from './state';
 // Create each component: 
 // - pass in the root element via querySelector
 // - pass any needed handler functions as properties of an actions object 
-
+const Scoreboard = createScoreboard(document.querySelector('#scoreboard-container'))
 // Roll-up display function that renders (calls with state) each component
 function display() {
     // Call each component passing in props that are the pieces of state this component needs
+    const scoreboardProps = { game: state.game };
+    Scoreboard(scoreboardProps);
 }
 
 // Call display on page load
---
+++ components/scoreboard.js
@@ -0,0 +1,11 @@
+export default function createScoreboard(root) { 
+    //reference DOM
+
+    //event listeners
+
+    //should return it's component render function
+    // return ({data}) => {
+    return (props) => {
+        const data = props.data;
+    };
+}
